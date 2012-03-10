45f5f4481ecaa1523dc1df9577826adafa34cc98
Josh Perez <josh@goatslacker.com>
2012-03-10 15:21:24 -0800

    Adds failing test for requires

--- test/unit/require.js
+++ test/unit/require.js
@@ -1,6 +1,8 @@
 (function () {
     "use strict";
     var coveraje = require('../..');
+    var fs = require('fs');
+    var path = require('path');
 
     suite('require', function () {
         test('relative path', function (done) {
@@ -12,6 +14,23 @@
                 {
                     globals: "node",
                     quiet: true
+                }
+            );
+        });
+
+        test('relative path2', function (done) {
+            coveraje.cover(
+                fs.readFileSync(path.join(__dirname, 'helper', 'require_relative1.js'), 'utf-8'),
+                function (context, inst) {
+                    if (context.foo === "test") done();
+                },
+                {
+                    globals: "node",
+                    quiet: true
+                },
+                function () { },
+                function (e, err) {
+                  throw e || err;
                 }
             );
         });
