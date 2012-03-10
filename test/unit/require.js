ef103916347e6bba4a10a55856c5f7b7de25bc7c
Josh Perez <josh@goatslacker.com>
2012-03-10 15:58:42 -0800

    Fixes GH-24.

--- test/unit/require.js
+++ test/unit/require.js
@@ -5,24 +5,11 @@
     var path = require('path');
 
     suite('require', function () {
-        test('relative path', function (done) {
-            coveraje.cover(
-                "var foo = require('./helper/require_relative1.js')",
-                function (context, inst) {
-                    if (context.foo === "test") done();
-                },
-                {
-                    globals: "node",
-                    quiet: true
-                }
-            );
-        });
-
         test('relative path2', function (done) {
             coveraje.cover(
-                fs.readFileSync(path.join(__dirname, 'helper', 'require_relative1.js'), 'utf-8'),
+                path.join(__dirname, 'helper', 'require_relative1.js'),
                 function (context, inst) {
-                    if (context.foo === "test") done();
+                    if (context.module.exports === "test") done();
                 },
                 {
                     globals: "node",
