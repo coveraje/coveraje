86dbdedc2673b2dd0a9381ab71ce8490e92874f5
Wolfgang Kluge <wolfgang@klugesoftware.de>
2012-03-11 22:32:19 +0100

    Add more require tests

--- test/unit/require.js
+++ test/unit/require.js
@@ -15,6 +15,47 @@
                 }
             );
         });
+
+        test('relative path (resolveRequired)', function (done) {
+            coveraje.cover(
+                "var foo = require('./helper/require_relative1.js')",
+                function (context, inst) {
+                    if (context.foo === "test") done();
+                },
+                {
+                    globals: "node",
+                    quiet: true,
+                    resolveRequired: ["*"]
+                }
+            );
+        });
+
+        test('npm installed', function (done) {
+            coveraje.cover(
+                "var foo = require('jshint')",
+                function (context, inst) {
+                    if (context.foo.JSHINT != null) done();
+                },
+                {
+                    globals: "node",
+                    quiet: true
+                }
+            );
+        });
+
+        test('npm installed (resolveRequired)', function (done) {
+            coveraje.cover(
+                "var foo = require('jshint')",
+                function (context, inst) {
+                    if (context.foo.JSHINT != null) done();
+                },
+                {
+                    globals: "node",
+                    quiet: true,
+                    resolveRequired: ["*"]
+                }
+            );
+        });
     });
 
 
